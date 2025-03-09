3b290f288eba1b3e94369ac4d62a995096e4c621
timothy <timothy.waweru1@student.moringaschool.com>
2025-03-09 17:40:32 +0300

    yehh

---
+++ index.js
@@ -0,0 +1,49 @@
+const expect = require("expect");
+
+1 + 80; 
+60 - 40; 
+2 * 3.4; 
+5.0 / 2.5; 
+
+function add (a,b) {
+    return a + b
+}
+add (1,80)
+function subtract (c,d) {
+    return c - d
+}
+subtract (60,40)
+function multiply (e,f) {
+    return e * f
+}
+multiply (2,3.4)
+function divide (g,h) {
+    return g / h
+}
+divide (5.0,2.5)
+
+function increment (z) {
+    return z += 1 
+}
+increment (4);
+
+function decrement (t) {
+    return t -= 1;
+}
+decrement (12);
+
+
+
+function makeInt (n,k) {
+    return (n.toString());
+}
+makeInt ("4",10);
+
+function isNaN (preserveDecimal) {
+    return true;
+}
+isNaN ('2.222');
+
+
+
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -40,10 +40,10 @@ describe('basic math functions', function () {
     expect(divide(a, b)).toEqual(a / b)
   })
   
-  it('increment(n) increments n and returns the result', function() {
+  it(
+    'increment(n) increments n and returns the result', function() {
     expect(increment(a)).toEqual(a + 1)
   })
-  
   it('decrement(n) decrements n and returns the result', function() {
     expect(decrement(a)).toEqual(a - 1)
   })
